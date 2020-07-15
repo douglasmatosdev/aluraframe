95a859cbb5dc7f4774e1de44b7144912629f51b7
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-15 20:47:50 -0300

    creating class Negociacao

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,12 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    obtemVolume() {
+        return this._quantidade * this._valor;
+    }
+}
